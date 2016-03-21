754eb88918541600acd1a86d444a4d6a80d35c7a
sujunhao <sujunhao.hit@gmail.com>
2016-03-21 22:42:18 +0800

    finish globalalignment class

--- test.cpp
+++ test.cpp
@@ -9,66 +9,6 @@
 // #include "global_alignment.h"
 using namespace std;
 
-int32_t gdb[100][100];
-
-string s1, s2;
-
-
-void get_global_alignment(size_t i, size_t j, vector<int> &v)
-{
-	if (i && j && gdb[i][j] == (gdb[i-1][j-1] + (s1[i-1] == s2[j-1]))) 
-	{
-		get_global_alignment(i-1, j-1, v);
-		v.push_back(0);
-		return;
-	}
-	if (i && gdb[i][j] == gdb[i-1][j])
-	{
-		get_global_alignment(i-1, j, v);
-		v.push_back(1);
-		return;
-	}
-	if (j && gdb[i][j] == gdb[i][j-1])
-	{
-		get_global_alignment(i, j-1, v);
-		v.push_back(2);
-		return;
-	}
-	if (i) 
-	{
-		get_global_alignment(i-1, j, v);
-		v.push_back(1);
-		return;
-	}
-	if (j) 
-	{
-		get_global_alignment(i, j-1, v);
-		v.push_back(2);
-		return;
-	}
-	v.push_back(0);
-	return;
-
-}
-
-void Global_Alignment(const std::string s1, const std::string s2)
-{
-	int32_t tmp = 0;
-
-	for (size_t i = 0; i <= s1.size(); ++i)
-	{
-		for (size_t j = 0; j <= s2.size(); ++j)
-		{
-			tmp = 0;
-			if (i) tmp = std::max(tmp, gdb[i-1][j]);
-			if (j) tmp = std::max(tmp, gdb[i][j-1]);
-			if (i > 0 && j > 0)	tmp = std::max(tmp, gdb[i-1][j-1] + (s1[i-1] == s2[j-1]));
-			gdb[i][j] = tmp;
-			printf("%d ", gdb[i][j]);
-		}
-		printf("\n");
-	}
-}
 
 class global_alignment
 {
@@ -82,51 +22,182 @@ class global_alignment
 		}
 		global_alignment()
 		{
-			
 		}
-		void set_string(string s11, string s22, string &s3, string &s4)
+		void set(string s11, string s22, string &s3, string &s4)
 		{
 			s1 = s11;
 			s2 = s22;
-			s_1 = s3;
-			s_2 = s4;
+			cout << s1 << endl << s2 << endl;
+			set_dp();
+			v.clear();
+			get_dp(s1.size(), s2.size(), v);
+
+			s_1.clear();
+			s_2.clear();
+
+
+			for (size_t i=0, m=0, n=0; i<v.size(); ++i) 
+			{
+				switch(v[i])
+				{
+					case 0:
+						s_1.append(s1, m++, 1);
+						s_2.append(s2, n++, 1);
+						break;
+					case 1:
+						s_1.append(s1, m++, 1);
+						s_2.append("_");
+						break;
+					case 2:
+						s_1.append("_");
+						s_2.append(s2, n++, 1);
+						break;
+
+				}
+				// printf("%d ", v[i]);
+			}
+			s3 = s_1;
+			s4 = s_2;
+			// cout << endl << s_1 << endl << s_2 << endl;
 		}
 
+		void set(string s11, string s22)
+		{
+			s1 = s11;
+			s2 = s22;
+			cout << s1 << endl << s2 << endl;
+			set_dp();
+			v.clear();
+			get_dp(s1.size(), s2.size(), v);
+
+			s_1.clear();
+			s_2.clear();
+
+			for (size_t i=0, m=0, n=0; i<v.size(); ++i) 
+			{
+				switch(v[i])
+				{
+					case 0:
+						s_1.append(s1, m++, 1);
+						s_2.append(s2, n++, 1);
+						break;
+					case 1:
+						s_1.append(s1, m++, 1);
+						s_2.append("_");
+						break;
+					case 2:
+						s_1.append("_");
+						s_2.append(s2, n++, 1);
+						break;
+
+				}
+				// printf("%d ", v[i]);
+			}
+			cout << endl << s_1 << endl << s_2 << endl;
+		}
+
+
+		void set_dp()
+		{
+			int32_t tmp = 0;
+
+			for (size_t i = 0; i <= s1.size(); ++i)
+			{
+				for (size_t j = 0; j <= s2.size(); ++j)
+				{
+					tmp = 0;
+					if (i) tmp = std::max(tmp, dp[i-1][j]);
+					if (j) tmp = std::max(tmp, dp[i][j-1]);
+					if (i > 0 && j > 0)	tmp = std::max(tmp, dp[i-1][j-1] + (s1[i-1] == s2[j-1]));
+					dp[i][j] = tmp;
+					// printf("%d ", dp[i][j]);
+				}
+				// printf("\n");
+			}
+		}
+
+
+		void get_dp(size_t i, size_t j, vector<int> &v)
+		{
+			if (i && j && dp[i][j] == (dp[i-1][j-1] + (s1[i-1] == s2[j-1]))) 
+			{
+				get_dp(i-1, j-1, v);
+				v.push_back(0);
+				return;
+			}
+			if (i && dp[i][j] == dp[i-1][j])
+			{
+				get_dp(i-1, j, v);
+				v.push_back(1);
+				return;
+			}
+			if (j && dp[i][j] == dp[i][j-1])
+			{
+				get_dp(i, j-1, v);
+				v.push_back(2);
+				return;
+			}
+			if (i) 
+			{
+				get_dp(i-1, j, v);
+				v.push_back(1);
+				return;
+			}
+			if (j) 
+			{
+				get_dp(i, j-1, v);
+				v.push_back(2);
+				return;
+			}
+			v.push_back(0);
+			return;
+		}
+
+
+
+	private:
+		int32_t dp[100][100];
+		vector<int> v;
+		string s1, s2, s_1, s_2;
 };
 
 
 int main()
 {
-	s1 = "AATTCCCCTAAAA";
-	s2 = "TATTGCAGCTTAATTTTTT";
-	cout << s1 << s2 << endl;
-	Global_Alignment(s1, s2);
-	string s[2];
+	string s1 = "AAAAGGGTTGTTACCCG";
+	string s2 = "ATAAGGTTGTTACCC";
+	string s3, s4;
+	global_alignment ga;
+	ga.set(s1, s2, s3, s4);
+	cout << endl <<s3 << endl << s4 << endl;
+	// cout << s1 << s2 << endl;
+	// Global_Alignment(s1, s2);
+	// string s[2];
 
-	vector<int> v;
-	get_global_alignment(s1.size(), s2.size(), v);
+	// vector<int> v;
+	// get_global_alignment(s1.size(), s2.size(), v);
 
-	for (size_t i=0, m=0, n=0; i<v.size(); ++i) 
-	{
-		switch(v[i])
-		{
-			case 0:
-				s[0].append(s1, m++, 1);
-				s[1].append(s2, n++, 1);
-				break;
-			case 1:
-				s[0].append(s1, m++, 1);
+	// for (size_t i=0, m=0, n=0; i<v.size(); ++i) 
+	// {
+	// 	switch(v[i])
+	// 	{
+	// 		case 0:
+	// 			s[0].append(s1, m++, 1);
+	// 			s[1].append(s2, n++, 1);
+	// 			break;
+	// 		case 1:
+	// 			s[0].append(s1, m++, 1);
 
-				s[1].append("_");
-				break;
-			case 2:
-				s[1].append(s2, n++, 1);
-				s[0].append("_");
-				break;
+	// 			s[1].append("_");
+	// 			break;
+	// 		case 2:
+	// 			s[1].append(s2, n++, 1);
+	// 			s[0].append("_");
+	// 			break;
 
-		}
-		printf("%d ", v[i]);
-	}
-	cout << endl << s[0] << endl << s[1] << endl;
+	// 	}
+	// 	printf("%d ", v[i]);
+	// }
+	// cout << endl << s[0] << endl << s[1] << endl;
 	return 0;
 }
