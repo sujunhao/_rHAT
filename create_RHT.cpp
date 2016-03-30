d3b474dde5a6b77ac26bc9654d747bb8e54da186
sujunhao <sujunhao.hit@gmail.com>
2016-03-30 22:13:45 +0800

    update create_rht

--- create_RHT.cpp
+++ create_RHT.cpp
@@ -8,28 +8,58 @@
 #include "RHT.h"
 using namespace std;
 
-// #define PRINT_WINDOW_INDEX
+// #define PRINTLOG
 
 int main(int argc, char** argv) 
 {
 
-    
+    int c;
+    opterr = 0;
+    while ((c = getopt (argc, argv, "p:w:")) != -1) 
+    {
+        switch (c)
+        {
+            case 'p':
+                if (optarg)
+                    PointerListLen = atol(optarg);
+                break;
+            case 'w':
+                if (optarg)
+                    WindowListLen = atol(optarg);
+                break;
+            default:
+                abort ();
+        }
+    }
+    std::cout << PointerListLen << " " << WindowListLen << "\n";
+
+
     ifstream inf;
     inf.open("E.coli.fa");
 
     FILE *pout;
     pout = fopen("out_RHT", "wb");
 
+    #ifdef PRINTLOG
+    ofstream out;
+    out.open("out_R");
+    #endif
+
+
     string dna_name, dna_s, dna_w;
 
     getline(inf, dna_name);
-
     while (inf >> dna_s) dna_w.append(dna_s);
+
     uint32_t tmp = 0, tar;
 
     //init mask
     MASK = MASK >> (32 - PointerListLen * 2);
-    dna_bitset db(PointerListLen);
+
+    // std::cout << dna_w.size() << endl;
+    
+    //init rht table with dna_size
+    RHT db(dna_w.size());
 
 
     for (size_t i=0; i<dna_w.size(); ++i)
@@ -65,10 +95,16 @@ int main(int argc, char** argv)
         }
     } 
 
-   
-    db.write_hash_out(pout);
+    db.create_p_w();
+    db.write_hash(pout);
+
+    #ifdef PRINTLOG
+    db.write_hash_test(out);
+    out.close();
+    #endif
+
+
     fclose(pout);
-    
     inf.close();
 
     printf("Time used = %.2f\n",  (double)clock() / CLOCKS_PER_SEC);
